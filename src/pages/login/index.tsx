6f2db791b7eed29fbc7ce7e7ab7a84f01085f564
Rafael Garcia <mroshi1@github.com>
2021-09-03 10:12:14 -0300

    Finalizacao tela de cadastro

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -10,7 +10,7 @@ export default function Login() {
 
   return (
     <>
-      {/* <main className={styles.containerSignIn}>
+      <main className={styles.containerSignIn}>
         <section className={styles.left}>
           <Image
             width="655.14"
@@ -60,23 +60,66 @@ export default function Login() {
             </form>
           </div>
         </section>
-      </main> */}
-      <main className="styles.containerSignIn">
+      </main>
+      {/* <main className={styles.containerSignUp}>
         <section className={styles.left}>
-          <Image width="655.14"
-          height="523.63"
-          src="/imgs/createIllustration.svg"
-          alt="Cara aleatorio"
+          <Image
+            width="655.14"
+            height="523.63"
+            src="/imgs/createIllustration.svg"
+            alt="Cara aleatorio"
           />
         </section>
         <section className={styles.right}>
+          <div className={styles.haveAccount}>
+            <span> Já tem uma conta </span>
+            <button>ENTRAR</button>
+          </div>
           <div className={styles.rightContent}>
-
-            
+            <h1>Bem Vindo ao Rajovi-Platform</h1>
+            <span>Crie já sua conta</span>
+            <form className={styles.registerForm}>
+              <Input
+                label="Nome Completo" //perguntar para o joão quanto ao value e setValue
+                type="text"
+                id="name"
+                placeHolder="Insrira seu nome completo"
+                // value ={name}
+                error={false}
+                required
+              />
+              <div>
+                <Input
+                  label="E-mail"
+                  type="text"
+                  id="email"
+                  placeholder="Insira seu e-mail"
+                  value={email}
+                  setValue={setEmail}
+                  error={false}
+                  required
+                />
+              </div>
+              <div>
+                <Input
+                  label="Senha"
+                  type="passowrd"
+                  id="password"
+                  placeholder="Insira sua senha"
+                  value={password}
+                  setValue={setPassword}
+                  error={false}
+                  required
+                />
+              </div>
+              <div className={styles.registerBtn}>
+                <button>Criar Conta</button>
+                
+              </div>
+            </form>
           </div>
         </section>
-        
-      </main>
+      </main> */}
     </>
   );
 }
