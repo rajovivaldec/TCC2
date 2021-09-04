6ef581019214effcd3f01c8b3304b1c6fc5c62ff
Joao Fernando <joaofbrigido@hotmail.com>
2021-09-04 10:44:02 -0300

    Menu Finalizado

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,58 +9,56 @@ export default function Login() {
   const [password, setPassword] = useState("");
 
   return (
-    <>
-      <main className={styles.containerSignIn}>
-        <section className={styles.left}>
-          <Image
-            width="655.14"
-            height="523.63"
-            src="/imgs/loginIllustratios.svg"
-            alt="Professor em frente a uma lousa"
-          />
-        </section>
+    <main className={styles.containerSignIn}>
+      <section className={styles.left}>
+        <Image
+          width="655.14"
+          height="523.63"
+          src="/imgs/loginIllustratios.svg"
+          alt="Professor em frente a uma lousa"
+        />
+      </section>
 
-        <section className={styles.right}>
-          <div className={styles.createAccount}>
-            <span>Não tem um conta?</span>
-            <button>Criar Conta</button>
-          </div>
+      <section className={styles.right}>
+        <div className={styles.createAccount}>
+          <span>Não tem um conta?</span>
+          <button>Criar Conta</button>
+        </div>
 
-          <div className={styles.rightContent}>
-            <h1>Bem Vindo de volta!</h1>
-            <span>Faça o Login com suas credenciais</span>
+        <div className={styles.rightContent}>
+          <h1>Bem Vindo de volta!</h1>
+          <span>Faça o Login com suas credenciais</span>
 
-            <form className={styles.form}>
+          <form className={styles.form}>
+            <Input
+              label="E-mail"
+              type="text"
+              id="email"
+              placeholder="Insira seu e-mail"
+              value={email}
+              setValue={setEmail}
+              error={false}
+              required
+            />
+            <div>
               <Input
-                label="E-mail"
-                type="text"
-                id="email"
-                placeholder="Insira seu e-mail"
-                value={email}
-                setValue={setEmail}
+                label="Senha"
+                type="password"
+                id="password"
+                placeholder="Insira sua senha"
+                value={password}
+                setValue={setPassword}
                 error={false}
                 required
               />
-              <div>
-                <Input
-                  label="Senha"
-                  type="password"
-                  id="password"
-                  placeholder="Insira sua senha"
-                  value={password}
-                  setValue={setPassword}
-                  error={false}
-                  required
-                />
-              </div>
-              <div className={styles.btnsWrapper}>
-                <button>Entrar</button>
-                <button>Esqueceu a senha?</button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
-    </>
+            </div>
+            <div className={styles.btnsWrapper}>
+              <button>Entrar</button>
+              <button>Esqueceu a senha?</button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
   );
 }
