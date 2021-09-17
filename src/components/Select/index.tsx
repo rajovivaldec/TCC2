bd289e6cd0cb58ca66b101cba40a1fac642190cc
Joao Fernando <joaofbrigido@hotmail.com>
2021-09-16 21:15:42 -0300

    component checkbox e select finalizados

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,35 @@
+import styles from "./styles.module.scss";
+
+export const Select = ({
+  id,
+  label,
+  options,
+  value,
+  setValue,
+  defaultValue,
+}) => {
+  return (
+    <>
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+      <select
+        className={styles.select}
+        id={id}
+        value={value}
+        onChange={({ target }) => setValue(target.value)}
+      >
+        <option disabled value="">
+          {defaultValue}
+        </option>
+        {options.map((option, idx) => {
+          return (
+            <option value={option.id} key={idx}>
+              {option.nome}
+            </option>
+          );
+        })}
+      </select>
+    </>
+  );
+};
